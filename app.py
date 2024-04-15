f67309b2e40849f375ea768a4ef02a1bb13844f0
finuaz <imans.office@gmail.com>
2024-04-15 15:22:25 +0700

    config app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_smorest import Api
 from dotenv import load_dotenv
 from db import db
 from psycopg2 import _psycopg
+from flask_jwt_extended import JWTManager
 
 
 def create_app(is_test=False):
@@ -12,16 +13,40 @@ def create_app(is_test=False):
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 
-    # "postgresql://postgres.ihkeizttbzwdzsyrgade:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
-
     db.init_app(app)
 
-    class SupaUser(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(80), unique=True, nullable=False)
-        email = db.Column(db.String)
+    # class SupaUser(db.Model):
+    #     id = db.Column(db.Integer, primary_key=True)
+    #     username = db.Column(db.String(80), unique=True, nullable=False)
+    #     email = db.Column(db.String)
 
     with app.app_context():
         db.create_all()
+
+    app.config["DEBUG"] = True
+
+    jwt = JWTManager(app)
+
+    app.config["JWT_SECRET_KEY"] = "tet tet toooettt 1000x"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 36000  #  Expires in 10 hours
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 2592000  #  Expires in 30 days
+
+    @jwt.expired_token_loader
+    def expired_token_callback(expired_token, jwt_data):
+        return jsonify({"message": "Token has expired"}), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(_):
+        return jsonify({"message": "Invalid token"}), 400
+
+    @jwt.unauthorized_loader
+    def unauthorized_callback(_):
+        return jsonify({"message": "Unauthorized access"}), 401
+
+    @jwt.needs_fresh_token_loader
+    def needs_fresh_token_callback(_):
+        return jsonify({"message": "Fresh token required"}), 401
+
+    api = Api(app)
 
     return app
