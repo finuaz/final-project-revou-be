9b3a710f6a55d5c953476210a9e4d40c76323e75
finuaz <imans.office@gmail.com>
2024-04-24 16:50:42 +0700

    finishing user social

--- controllers/user_social.py
+++ controllers/user_social.py
@@ -66,17 +66,8 @@ class BatchConnect(MethodView):
                 abort(500, description=f"Failed to update recipe information: {str(e)}")
 
 
-@blp.route("/users/socials/batch/disconnect")
-class BatchDisconnect(MethodView):
-    @blp.arguments(UserSocialsSchema)
-    @blp.response(201, UserSocialsSchema)
-    @jwt_required()
-    def post(self, socials_data):
-        return
-
-
 @blp.route("/users/profile/socials")
-class SocialDisplay(MethodView):
+class SocialList(MethodView):
     @blp.response(201, UserSocialsSchema)
     @jwt_required()
     def get(self):
@@ -84,7 +75,7 @@ class SocialDisplay(MethodView):
             current_user_id = get_jwt_identity()["id"]
             social = SocialModel.query.filter_by(user_id=current_user_id).first()
             if not social:
-                abort(404, "User Socials not found")
+                return jsonify({"message": "user has no linked socials"}), 404
 
             serialized_user = UserSocialsSchema().dump(social)
             return jsonify(serialized_user), 200
@@ -96,13 +87,29 @@ class SocialDisplay(MethodView):
             abort(500, "Internal Server Error")
 
 
-@blp.route("/users/socials/<int:user_id_in_search>")
+@blp.route("/users/<int:user_id_in_search>/socials")
 class SocialDisplay(MethodView):
-    @blp.arguments(UserSocialsSchema)
     @blp.response(201, UserSocialsSchema)
-    @jwt_required()
-    def post(self, user_id_in_search):
-        return
+    def get(self, user_id_in_search):
+
+        try:
+            user = UserModel.query.filter_by(id=user_id_in_search).first()
+            social = SocialModel.query.filter_by(user_id=user_id_in_search).first()
+
+            if not user:
+                return jsonify({"message": "user does not exist"}), 404
+
+            if not social:
+                return jsonify({"message": "user has no linked socials"}), 404
+
+            serialized_user = UserSocialsSchema().dump(social)
+            return jsonify(serialized_user), 200
+        except SQLAlchemyError as e:
+            current_app.logger.error(f"Database error: {str(e)}")
+            abort(500, "Internal Server Error")
+        except Exception as e:
+            current_app.logger.error(f"An unexpected error occurred: {str(e)}")
+            abort(500, "Internal Server Error")
 
 
 @blp.route("/users/socials/facebook/connect")
@@ -121,7 +128,7 @@ class FacebookConnect(MethodView):
                     user_id=user_id,
                     facebook=socials_data["facebook"],
                 )
-                social.add_user_social
+                social.add_user_social()
             except IntegrityError:
                 abort(400, message="recipe with that title already exists")
             except SQLAlchemyError as e:
@@ -150,7 +157,35 @@ class InstagramConnect(MethodView):
     @blp.response(201, UserSocialsSchema)
     @jwt_required()
     def post(self, socials_data):
-        return
+        user_id = get_jwt_identity()["id"]
+        social = SocialModel.query.filter_by(user_id=user_id).first()
+
+        if not social:
+            try:
+                social = SocialModel(
+                    user_id=user_id,
+                    instagram=socials_data["instagram"],
+                )
+                social.add_user_social()
+            except IntegrityError:
+                abort(400, message="recipe with that title already exists")
+            except SQLAlchemyError as e:
+                abort(
+                    500,
+                    message=f"An error occurred while creating the recipe: {str(e)}",
+                )
+            return social
+
+        else:
+            try:
+
+                social.update_user_social(socials_data)
+                return UserSocialsSchema().dump(social), 200
+            except Forbidden as e:
+                abort(403, description=str(e))
+
+            except Exception as e:
+                abort(500, description=f"Failed to update recipe information: {str(e)}")
 
 
 @blp.route("/users/socials/tiktok/connect")
@@ -160,7 +195,35 @@ class TiktokConnect(MethodView):
     @blp.response(201, UserSocialsSchema)
     @jwt_required()
     def post(self, socials_data):
-        return
+        user_id = get_jwt_identity()["id"]
+        social = SocialModel.query.filter_by(user_id=user_id).first()
+
+        if not social:
+            try:
+                social = SocialModel(
+                    user_id=user_id,
+                    tiktok=socials_data["tiktok"],
+                )
+                social.add_user_social()
+            except IntegrityError:
+                abort(400, message="recipe with that title already exists")
+            except SQLAlchemyError as e:
+                abort(
+                    500,
+                    message=f"An error occurred while creating the recipe: {str(e)}",
+                )
+            return social
+
+        else:
+            try:
+
+                social.update_user_social(socials_data)
+                return UserSocialsSchema().dump(social), 200
+            except Forbidden as e:
+                abort(403, description=str(e))
+
+            except Exception as e:
+                abort(500, description=f"Failed to update recipe information: {str(e)}")
 
 
 @blp.route("/users/socials/facebook/disconnect")
@@ -170,7 +233,21 @@ class FacebookDisconnect(MethodView):
     @blp.response(201, UserSocialsSchema)
     @jwt_required()
     def post(self, socials_data):
-        return
+        user_id = get_jwt_identity()["id"]
+        social = SocialModel.query.filter_by(user_id=user_id).first()
+
+        if not social:
+            return jsonify({"message": "user has no linked socials"}), 404
+        else:
+            try:
+                social.facebook = None
+                social.update_user_social(social.__dict__)
+                return UserSocialsSchema().dump(social), 200
+            except Forbidden as e:
+                abort(403, description=str(e))
+
+            except Exception as e:
+                abort(500, description=f"Failed to update recipe information: {str(e)}")
 
 
 @blp.route("/users/socials/instagram/disconnect")
@@ -180,7 +257,21 @@ class InstagramDisconnect(MethodView):
     @blp.response(201, UserSocialsSchema)
     @jwt_required()
     def post(self, socials_data):
-        return
+        user_id = get_jwt_identity()["id"]
+        social = SocialModel.query.filter_by(user_id=user_id).first()
+
+        if not social:
+            return jsonify({"message": "user has no linked socials"}), 404
+        else:
+            try:
+                social.instagram = None
+                social.update_user_social(social.__dict__)
+                return UserSocialsSchema().dump(social), 200
+            except Forbidden as e:
+                abort(403, description=str(e))
+
+            except Exception as e:
+                abort(500, description=f"Failed to update recipe information: {str(e)}")
 
 
 @blp.route("/users/socials/tiktok/disconnect")
@@ -190,4 +281,18 @@ class TiktokDisconnect(MethodView):
     @blp.response(201, UserSocialsSchema)
     @jwt_required()
     def post(self, socials_data):
-        return
+        user_id = get_jwt_identity()["id"]
+        social = SocialModel.query.filter_by(user_id=user_id).first()
+
+        if not social:
+            return jsonify({"message": "user has no linked socials"}), 404
+        else:
+            try:
+                social.tiktok = None
+                social.update_user_social(social.__dict__)
+                return UserSocialsSchema().dump(social), 200
+            except Forbidden as e:
+                abort(403, description=str(e))
+
+            except Exception as e:
+                abort(500, description=f"Failed to update recipe information: {str(e)}")
