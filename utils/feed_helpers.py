89324c473112a19d68eb3941a41c1db902ae04f8
finuaz <imans.office@gmail.com>
2024-05-03 20:19:25 +0700

    adding controller for feed

--- utils/feed_helpers.py
+++ utils/feed_helpers.py
@@ -14,36 +14,41 @@ from db import db
 
 
 def find_all_category(recipe_id):
-    recipe_category = RecipeCategoryRelationModel.query.filter_by(
+    categories = []
+    recipe_categories = RecipeCategoryRelationModel.query.filter_by(
         recipe_id=recipe_id
     ).all()
-    if recipe_category:
+    for recipe_category in recipe_categories:
         category = CategoryModel.query.get(recipe_category.category_id)
-        return category.category
-    else:
-        return None
+        categories.append(category.category)
+    return categories if categories else None
 
 
 def find_all_type(recipe_id):
-    recipe_type = RecipeTypeRelationModel.query.filter_by(recipe_id=recipe_id).all()
+    recipe_type = RecipeTypeRelationModel.query.filter_by(recipe_id=recipe_id).first()
     if recipe_type:
         type = TypeModel.query.get(recipe_type.type_id)
-        return type.type
-    else:
-        return None
+        return type.type if type else None
 
 
 def find_all_origin(recipe_id):
-    recipe_origin = RecipeOriginRelationModel.query.filter_by(recipe_id=recipe_id).all()
+    print("-------------------- wait")
+    recipe_origin = RecipeOriginRelationModel.query.filter_by(
+        recipe_id=recipe_id
+    ).first()
     if recipe_origin:
+        print("-------------------- here")
         origin = OriginModel.query.get(recipe_origin.origin_id)
         print(origin)
-        return origin.origin
-    else:
-        return None
+        return origin.origin if origin else None
+    # else:
+    #     return None
 
 
 def find_all_tag(recipe_id):
+    tags = []
     recipe_tags = RecipeTagRelationModel.query.filter_by(recipe_id=recipe_id).all()
-    tags = [TagModel.query.get(recipe_tag.tag_id).tagname for recipe_tag in recipe_tags]
-    return tags
+    for recipe_tag in recipe_tags:
+        tag = TagModel.query.get(recipe_tag.tag_id)
+        tags.append(tag.tagname)
+    return tags if tags else None
